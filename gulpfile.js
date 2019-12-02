2458e2e7f053a031130d28cf15b6fca8249a27c5
Narumi <narumi@Narumis-MBP.mediegruppen.se>
2019-12-02 13:48:35 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+
+gulp.task('sass', function(cb) {
+  gulp
+    .src('scss/*.scss')
+    
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(
+      gulp.dest("css")
+      )
+    
+    cb();
+});
+
+gulp.task(
+  'default',
+  gulp.series('sass', function(cb) { 
+    gulp.watch('*.scss', gulp.series('sass'));
+    cb();
+  })
+);
