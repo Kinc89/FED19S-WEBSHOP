5f20137534e48bc5827ecfc35d9ea803d9a8baae
Julia Nilsson <julianilsson@Julias-Air.mediegruppen.se>
2019-12-16 14:06:35 +0100

    fixad divar på cart

--- js/cart.js
+++ js/cart.js
@@ -6,14 +6,35 @@ $(document).ready(function(){
     // Loopa igenom listan för att kunna printa ut våra objekt.
     $.each(cart, function(i, currentCartItem){
         console.log(currentCartItem);
-        let imgcontainer = $("<div>").appendTo(".cartProductDiv").addClass("cartProductImg");
+        let imgcontainer = $("<div>").appendTo(".cartProductDiv").addClass("cartProductImg col-4");
         $("<img>")
             .appendTo(imgcontainer)
             .addClass("cartProductPicture img-fluid")
             .attr("src", currentCartItem.product.img);
-       
-
         
+        let titlecontainer = $("<div>").appendTo(".cartProductDiv").addClass("cartProductInfo col-6");
+        $("<h4>")
+            .appendTo(titlecontainer)
+            .addClass("")
+            .text(currentCartItem.product.title);
+        $("<p>")
+            .appendTo(titlecontainer)
+            .addClass("")
+            .text("Price: " + currentCartItem.product.price);
+        $("<p>")
+            .appendTo(titlecontainer)
+            .addClass("")
+            .text("Articlenr: " + currentCartItem.product.articlenr);
+
+        let deleteBtn = $("<div>").appendTo(".cartProductDiv").addClass("cartProductDeleteBtn col-2");
+        $("<button>")
+            .appendTo(deleteBtn)
+            .addClass("btn cartDeleteBtn")
+            .text("\u00D7");
+
+        $(".cartDeleteBtn").on("click", function(){
+
+        });
         
         // let h1HeaderDiv = $("<h1>").html("Say it with flowers").appendTo($("#carusellDiv"));
         // $(h1HeaderDiv).addClass("carusel_h1");
