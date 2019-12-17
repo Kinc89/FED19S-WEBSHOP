129ad5aa794463a72c5c15ad6f1dccfc7a4eab5f
Julia Nilsson <julianilsson@Julias-Air.mediegruppen.se>
2019-12-17 15:56:26 +0100

    Loading page

--- js/thank_you_page.js
+++ js/thank_you_page.js
@@ -1,11 +1,22 @@
+
+
 $(document).ready(function(){
+
+    setTimeout(function() {
+        $('.loadingWrapper').hide();
+    }, 2500); 
     
+    $('.container-fluid').hide();
+
+    setTimeout(function() {
+        $('.container-fluid').show();
+    }, 2500); 
+
     let newOrdernr = Math.floor(Math.random()* 5);
     let orderNr = $(".randomOrderNr").html(newOrdernr);
     //let selectedOrderNr = orderNr.val;
 
     
-
 });
 
 // grekiskan
