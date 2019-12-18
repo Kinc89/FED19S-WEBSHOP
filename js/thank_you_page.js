6294e26bc8f7d636e50af5518bd60f44de832a71
Narumi <narumi@Narumis-MBP.mediegruppen.se>
2019-12-18 12:57:01 +0100

    Structure

--- js/thank_you_page.js
+++ js/thank_you_page.js
@@ -7,23 +7,12 @@ $(document).ready(function(){
     setTimeout(function() {
         $('.container-fluid').show();
     }, 2500); 
-    
-  
-    
-  
-    
     let uniqueOrderNr = Math.floor((Math.random() * 999999) + 100);
     let placedOrders = JSON.parse(localStorage.getItem("Placed orders")) || [];
-
-    
     while (placedOrders.indexOf(uniqueOrderNr) >= 0) {
         uniqueOrderNr = Math.floor((Math.random() * 999999) + 100);
-
     }    
-    
     placedOrders.push(uniqueOrderNr);
     localStorage.setItem("Placed orders", JSON.stringify(placedOrders));
-
     $(".randomOrderNr").html(uniqueOrderNr);
-
 });
